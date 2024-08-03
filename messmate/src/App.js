bb17d4ffe075c7983e06773cba2fe8aa71549caf
Shrejalraut0746 <raut23796@gmail.com>
2024-08-03 13:14:05 +0530

    messmate updated

---
+++ messmate/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import PersistentLogin from "./Auth/PersistentLogin";
+import RequireAuth from "./Auth/RequireAuth";
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PersistentLogin />}>
+            <Route path="" element={<Home />}></Route>
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
