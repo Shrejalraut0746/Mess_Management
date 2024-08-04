6146256960003902b1135bd3be20b828485488cc
Shrejalraut0746 <raut23796@gmail.com>
2024-08-04 19:33:38 +0530

    Main file update

--- messmate/src/Components/Main.jsx
+++ messmate/src/Components/Main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx"
 
 const Main=()=> {
   return (
     <div className="flex flex-col content-between justify-between min-h-[100vh] bg-gradient-to-t bg-slate-200  ">
       <Navbar/>
       <Outlet className="mt-32"></Outlet>
-      
+      <Footer/>
     </div>
   );
 };
