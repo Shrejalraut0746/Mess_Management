3f29b2dae88a67812b2e1637ba0038ac4e884184
Aryanchavan0746 <gajananchavan024@gmail.com>
2024-08-04 18:22:55 +0530

    component files

--- messmate/src/App.js
+++ messmate/src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import PersistentLogin from "./Auth/PersistentLogin";
 import RequireAuth from "./Auth/RequireAuth";
 import Home from './Components/Home';
+import MenuHome from "./Components/Menu";
+import Contact from "./Components/Contact";
+import Aboutus from "./Components/Aboutus";
+import Unauthorized from "./Components/Unauthorized";
 
 function App() {
   return (
     <div className="App">
+     
       <BrowserRouter>
         <Routes>
           <Route element={<PersistentLogin />}>
+
             <Route path="" element={<Home />}></Route>
-
+            <Route path="/menu" element={<MenuHome />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/about" element={<Aboutus />}></Route>
           </Route>
-
+          <Route path="/unauthorized" element={<Unauthorized />}> </Route>
         </Routes>
       </BrowserRouter>
+      
     </div>
   );
 }
